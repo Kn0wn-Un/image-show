7dd9a57143cb63c99df0944947db0fd151ccdf36
Kn0wn-Un <naikdarsh175@gmail.com>
2020-11-04 10:06:42 +0530

    navbar js

---
+++ src/navbar.js
@@ -0,0 +1,21 @@
+import './style.css';
+
+const navBar = (() => {
+    const addNav = () => {
+        const nav = document.createElement('div');
+        nav.classList.add('navbar');
+        const bigI = document.createElement('h1');
+        bigI.innerHTML = 'I';
+        const bigS = document.createElement('h1');
+        bigS.innerHTML = 'S';
+        const a = document.createElement('a');
+        a.href = 'https://github.com/Kn0wn-Un/image-show';
+        a.innerHTML = 'GitHub';
+        nav.innerHTML = bigI.outerHTML + 'mage' + bigS.outerHTML + 'how';
+        nav.appendChild(a);
+        document.body.appendChild(nav);
+    };
+    return { addNav };
+})();
+
+export default navBar;
